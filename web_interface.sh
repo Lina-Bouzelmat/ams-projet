3bd337d835d7a98ae62087ac254940d175e81de7
Lina-Bouzelmat <152987036+Lina-Bouzelmat@users.noreply.github.com>
2025-04-24 21:18:25 +0200

    ajout etat machine 2

--- web_interface.sh
+++ web_interface.sh
@@ -12,7 +12,16 @@ generate_webpage() {
     echo "<h2>Graphique d'évolution CPU & RAM</h2>"
     echo "<img src='data:image/png;base64,$(base64 -w 0 $IMG_PATH)' alt='Graphique CPU/RAM' width='800'>"
     echo "<hr>"
-    echo "<h3>Dernières valeurs</h3>"
+
+    echo "<h3>Dernières données de la Machine 2</h3>"
+    if [ -f /home/uapv2202351/scripts/data_from_machine2.txt ]; then
+        cat /home/uapv2202351/scripts/data_from_machine2.txt
+    else
+        echo "Aucune donnée reçue de la Machine 2."
+    fi
+    echo "<hr>"
+    
+    echo "<h3>Dernières valeurs machine Principale</h3>"
     
     sqlite3 /home/uapv2202351/data/systeme_monitor.db "SELECT * FROM system_metrics ORDER BY id DESC LIMIT 5;" > $TMP_DATA
 
